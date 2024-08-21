cdcc47ebf20c33b931a5c6198d146904de924136
GustavoCKruger <gustavo.kruger1601@gmail.com>
2024-08-21 15:08:17 -0300

    FEAT: Conclusão da página de login #7

--- src/components/LoginInfo.vue
+++ src/components/LoginInfo.vue
@@ -4,7 +4,30 @@
 
 <template>
     <form>
+        <div class="entrar">
+            <h1 class="tit">Entrar</h1>
+            <hr>
+            <p class="infoDados">Informe os seus dados abaixo para acessá-la.</p>
+        </div>
 
+        <div class="log">
+            <label for="" >Email:</label>
+            <input type="email" class="prench">
+            <label for="" >Senha:</label>
+            <input type="password" class="prench">
+        </div>
+
+        <div class="confirm">
+            <div class="checkboxLembrar">
+            <input type="checkbox" class="lembrar">
+            <label for="">Manter-me logado</label>
+            </div>
+            <input class="buttonLogin" type="button" value="Login">
+            <div class="esquecOrCadastro"> 
+            <a href="/" class="esq">Esqueci minha senha</a>
+            <p class="cadastro">Novo no Site !<a href="/">CADASTRA-SE</a></p>
+            </div>
+        </div>
     </form>
 </template>
 
@@ -16,5 +39,72 @@ form{
     max-height: min-content;
     margin: 100px 0px 100px 28%;
     background-color: #D9D9D9;
+}
+
+.confirm{
+   margin-left: 287px;
+
+}
+
+.tit{
+    margin-left: 340px;
+    padding-top: 20px;
+}
+
+hr{
+    max-width: 700px;
+}
+
+.infoDados{
+    margin-left: 229px;
+}
+
+.log{
+    max-width: 100px;
+    margin-left: 50px;
+    margin-top: 50px;
+    margin-bottom: 50px;
+}
+
+.prench{
+    min-width: 390px;
+    min-height: 25px;
+    border: none;
+    margin-top: 5px;
+}
+
+.checkboxLembrar{
+    display: flex;
+    padding-left: 13px;
+    font-size: 15px;
+}
+.lembrar{
+    margin-bottom: 18px;
+    background-color: #D9D9D9;
+}
+
+.buttonLogin{
+    min-width: 190px;
+    min-height: 35px;
+    max-width: min-content;
+    max-height: min-content;
+    background-color: #00233C;
+    color: #FFFF;
+    border-radius: 8px;
+}
+
+.esquecOrCadastro{
+    font-size: 11px;
+    margin-top: 15px;
+    margin-bottom: 10px;
+}
+
+.esq{
+    margin-left: 36px;
+}
+
+.cadastro{
+    margin-left: 17px;
+    margin-top: 15px;
 }
 </style>
--- src/views/LoginPag.vue
+++ src/views/LoginPag.vue
@@ -1,9 +1,11 @@
 <script setup>
 import FooterBar from "../components/FooterBar.vue";
 import LoginInfo from "@/components/LoginInfo.vue";
+import NavBar from '@/components/NavBar.vue';
 </script>
 
 <template>
+    <NavBar />
     <LoginInfo />
     <FooterBar />
 </template>
