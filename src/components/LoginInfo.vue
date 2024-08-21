031219664a15386461c89887092c7a72daf91a01
GustavoCKruger <gustavo.kruger1601@gmail.com>
2024-08-21 10:41:35 -0300

    FEAT: Criação da página de login(continuar) #7

---
+++ src/components/LoginInfo.vue
@@ -0,0 +1,20 @@
+<script setup>
+
+</script>
+
+<template>
+    <form>
+
+    </form>
+</template>
+
+<style scoped>
+form{
+    min-width: 800px;
+    min-height: 500px;
+    max-width: min-content;
+    max-height: min-content;
+    margin: 100px 0px 100px 28%;
+    background-color: #D9D9D9;
+}
+</style>
---
+++ src/views/LoginPag.vue
@@ -0,0 +1,13 @@
+<script setup>
+import FooterBar from "../components/FooterBar.vue";
+import LoginInfo from "@/components/LoginInfo.vue";
+</script>
+
+<template>
+    <LoginInfo />
+    <FooterBar />
+</template>
+
+<style scoped>
+
+</style>
