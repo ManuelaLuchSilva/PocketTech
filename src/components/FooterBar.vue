5decfbc5a9a68ffbbe755ed5daef6938d4511dd1
ManuelaLuchSilva <manuelaluchtenbergsilva.ifc@gmail.com>
2024-08-26 15:20:45 -0300

    Feat PagProdutosAdm, começo de tabela #11

---
+++ src/components/FooterBar.vue
@@ -0,0 +1,116 @@
+<script setup></script>
+<template>
+    <footer>
+        <div class="container-footer">
+            <div>
+                <ul>
+                    <li class="tema-footer">Institucional</li>
+                    <li>Quem somos</li>
+                    <li>Localização</li>
+                    <li>Nossas Lojas</li>
+                </ul>
+            </div>
+            <div>
+                <ul>
+                    <li class="tema-footer">Dúvidas</li>
+                    <li>Entrega</li>
+                    <li>Garantia</li>
+                    <li>Como Comprar</li>
+                    <li>Formas de Pagamento</li>
+                    <li>Sobre Boletos</li>
+                </ul>
+            </div>
+            <div>
+                <ul>
+                    <li class="tema-footer">Ajuda</li>
+                    <li>SAC</li>
+                    <li>Fale Conosco</li>
+                    <li>Termos de Aceite</li>
+                    <li>Políticas de Privacidade</li>
+                </ul>
+            </div>
+            <div class="redesSociais">
+                <p>Siga-nos nas redes sociais</p>
+                <div class="redesSociais-img">
+                    <a href="">
+                        <img src="https://s3-alpha-sig.figma.com/img/06aa/28d7/c8c904d459318bbfbd5f1d63481cbe68?Expires=1724630400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=jNlYxYAIL6oXywXec1T81Gmm3D4spKsTSMGfJEy0uVNU-6SWG-~3LgSwRGUr5cBwhMNmvkMbs4~a4bg5ytVVCmASgeWggllM4qri4k6CPVws3ij28cH30dkvI2ozuuNyZI6szaedXg3qBkAkvXwhti6rf2kJd1Zw49T824iITspoPnzxrvwHSeBb1N1rp-jerHgSkeJcv4VpPawn0S3ldyLw0tvdCwd9Ck46dI4jwi3qKob4MKNohXFIcstiEbMu0cmO9MESMlKhNFgrTKVzvylyZHPuieyTNiHoFGPm0nwVeGC8Loevu-hf5pSK9ObVhuZhowgdvgcnizkAP8s7Pw__"
+                            alt="logo Instagram" />
+                    </a>
+                    <a href="">
+                        <img src="https://s3-alpha-sig.figma.com/img/0b88/4ee4/8758293f8295dd2e89e78369c8a1caff?Expires=1724630400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=nIibkOy8vwUYQe6vmXfrt4vQw6QjzGlHhPmsIpf8A2AlQ8niLbkFNSrKLLh3dUrnciCwNhC~dCqgTes4ZXTnBbxoTsTLw8-ut1OhYTS-UBHo59ywtSwoT2TaFpAaqHW8Hcz4o~tpT88Sz6y8UUqecUKEd82gTIrabhaQfXD7M5OkYSFoZWGyHdyKfJyAmpGT4aoYrEs5BD80q9R6Xu2FAUNsg2jDn7K182wHerOQqUVYvca3mAqIZu8ZERlnC46XbbW0qUeNR2jd8NcxXeEELCaDkIBI2maiENN1YtakmKktOy8jn2jqXlxK2Rv-MoI-tP6z5KMeMEb3cbUe407uPA__"
+                            alt="logo Facebook" />
+                    </a>
+                    <a href="">
+                        <img src="https://s3-alpha-sig.figma.com/img/770d/8245/fc94cf3633ad15b8219761d1dbb9c8a0?Expires=1724630400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=j8pxLDMKOuPMd4S0hb3-nMHwtyh3P45XWh~aYtgyex~Zkody0trIKb4dn9eKNGyBe50YkVHg72yXlKxsBYTrNU-K6fy17spiXOCbCpFvLDqGhHES~uyVpZ05jdqCQVmlDlBaajl2gf9X4aEpOPL3FKgYeQ7jgjvbePQPx0NhBAKqcex-xz~LP2oQTsgZCw2yDigF~W56PUjkQRFVMG3x3HytD~H3FoRCgzvA5ZZ7LdstENZOsZE5F4mn-icp3e9GaWNFV-yBHHBbIlobQ67d-Yrlgn93MGqfrA0qeI88R6mJHG0QeR9eolFLaJXJZz9dAq0Wd9IDGlyRmLCEIJMUYQ__"
+                            alt="logo Twiter" />
+                    </a>
+                    <a href="">
+                        <img id="youtube"
+                            src="https://s3-alpha-sig.figma.com/img/56cb/394d/55fbcc0549ef62d512a89a9314816e20?Expires=1724630400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=puADaKYYw~P7AWxyfwNmYWLo4GKWAQ7mefXUWyYgSooB6ylW5o5-QVU-CaZA3QVacx3sTy-5tI~l~vizDunAI4QgQqlzbbShVKg8HylQ~ML61WyjjXceCEahZU-4Kkk1NVL1Hn0RDnlxK7EOH2Ao36THp1W-yD-d1hIhicUxBy0s4f1cXA4dHBct2G9mdeClYgXmuV-nDI1HIycZZn4IgnThYSPOPgE4DYH6O92d0CwZJblwY8PPJ8zAN~c8J-QA2kCeON8Zg5XbBn41XnYqgftuWH6CfDjIFehdQkNt1xqTSqRxzMB3gf1SYid0hIWxsGezKREH9A4J1YnZpLMNzg__"
+                            alt="logo Youtube" />
+                    </a>
+                </div>
+            </div>
+        </div>
+    </footer>
+</template>
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Abril+Fatface&family=Danfo&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Major+Mono+Display&family=Noto+Serif+JP:wght@200..900&family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap');
+
+.container-footer {
+    width: 100%;
+    height: 200px;
+    background-color: #00233c;
+    bottom: 0;
+    display: flex;
+    color: white;
+}
+
+.container-footer div {
+    margin-top: 15px;
+    margin-left: 100px;
+}
+
+.container-footer ul li {
+    list-style: none;
+}
+
+ul .tema-footer {
+    font-family: 'Inter', sans-serif;
+    font-size: 20px;
+    font-weight: 700;
+    text-align: left;
+}
+
+ul li {
+    font-family: 'Inter', sans-serif;
+    font-size: 15px;
+    font-weight: 400;
+    line-height: 25px;
+    text-align: left;
+}
+
+div img {
+    width: 40px;
+    height: 40px;
+}
+
+#youtube {
+    width: 50px;
+    height: 50px;
+}
+
+.redesSociais {
+    position: absolute;
+    right: 200px;
+}
+
+.redesSociais .redesSociais-img {
+    display: flex;
+    margin: 0;
+}
+
+.redesSociais-img img{
+    margin-right: 20px;
+}
+</style>
