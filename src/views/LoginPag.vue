640861dda2377e7e6dfb67529df4737fb44bd884
GustavoCKruger <gustavo.kruger1601@gmail.com>
2024-08-28 14:20:47 -0300

    FEAT: Colocada a Navbar na Pag. Login #7

--- src/views/LoginPag.vue
+++ src/views/LoginPag.vue
@@ -1,10 +1,11 @@
 <script setup>
+import NavbarCadastELogin from "@/components/NavbarCadastELogin.vue";
 import FooterBar from "../components/FooterBar.vue";
 import LoginInfo from "@/components/LoginInfo.vue";
 </script>
 
 <template>
-    <NavBar />
+    <NavbarCadastELogin />
     <LoginInfo />
     <FooterBar />
 </template>
