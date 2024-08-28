e6ce47be7ac4905f2849be4ab0458e3efc96eddd
ManuelaLuchSilva <manuelaluchtenebrgsilva.ifc@gmail.com>
2024-08-28 11:03:03 -0300

    Feat Inicio margem faturamento #12

--- src/views/AdmmargemfatuView.vue
+++ src/views/AdmmargemfatuView.vue
@@ -1,62 +1,73 @@
 <script setup>
+import SidebarAdm from '../components/SidebarAdm.vue'
+import NavbarAdm from '../components/NavbarAdm.vue'
+import Footerbar from '../components/FooterBar.vue'
 import { ref, computed } from 'vue'
 
-const data = ref(null)
-
-const entradas = ref([
-  {
+const entries = ref([
+{
+    id: 1,
     categoria: 'Reparo de Tela',
     valor: 150,
-    data: '2024-08-01'
+    data: '01-08-2024'
   },
   {
+    id: 2,
     categoria: 'Troca de Bateria',
     valor: 80,
-    data: '2024-08-02'
+    data: '02-08-2024'
   },
   {
+    id: 3,
     categoria: 'Reparo de Botões',
     valor: 100,
-    data: '2024-08-05'
+    data: '05-08-2024'
   },
   {
+    id: 4,
     categoria: 'Limpeza Interna',
     valor: 60,
-    data: '2024-08-08'
+    data: '08-08-2024'
   },
   {
+    id: 5,
     categoria: 'Substituição de Câmera',
     valor: 120,
-    data: '2024-08-10'
+    data: '10-08-2024'
   },
   {
+    id: 6,
     categoria: 'Instalação de Película',
     valor: 30,
-    data: '2024-08-12'
+    data: '12-08-2024'
   },
   {
+    id: 7,
     categoria: 'Acessórios Personalizados',
     valor: 45,
-    data: '2024-08-15'
+    data: '15-08-2024'
   },
   {
+    id: 8,
     categoria: 'Conserto de Conector de Carregamento',
     valor: 90,
-    data: '2024-08-17'
+    data: '17-08-2024'
   },
   {
+    id: 9,
     categoria: 'Configuração e Atualização de Software',
     valor: 50,
-    data: '2024-08-20'
+    data: '20-08-2024'
   },
   {
+    id: 10,
     categoria: 'Substituição de Placa-mãe',
     valor: 200,
-    data: '2024-08-25'
+    data: '25-08-2024'
   }
 ])
 
-const saidas = ref({
+const exits = ref({
   Custos: 0,
   Despesas: 0,
   'Outras Saídas': 0
@@ -64,7 +75,7 @@ const saidas = ref({
 
 const valorBruto = computed(() => {
   let totalEntradas = 0
-  for (const value of Object.values(entradas.value)) {
+  for (const value of Object.values(entries.value)) {
     totalEntradas += parseFloat(value)
   }
   return totalEntradas.toFixed(2)
@@ -72,22 +83,41 @@ const valorBruto = computed(() => {
 
 const valorLiquido = computed(() => {
   let totalSaidas = 0
-  for (const value of Object.values(saidas.value)) {
+  for (const value of Object.values(exits.value)) {
     totalSaidas += parseFloat(value)
   }
   return (valorBruto.value - totalSaidas).toFixed(2)
 })
+
+
+const selectedDate = ref(null)
+
+const filteredEntries = computed(() => {
+  if (!selectedDate.value) {
+    return entries.value
+  }
+  return entries.value.filter((entry) => entry.data === selectedDate.value)
+})
+
+const filteredExits = computed(() => {
+  if (!selectedDate.value) {
+    return exits.value
+  }
+  return exits.value.filter((exit) => exit.data === selectedDate.value)
+})
 </script>
 
 <template>
+  <NavbarAdm/>
   <div class="container">
+    <SidebarAdm />
     <div class="header">
       <h1>Gere a margem de faturamento</h1>
     </div>
     <div class="content">
       <div class="data">
         <label for="data">Informe a data:</label>
-        <input type="date" id="data" v-model="data" />
+        <input type="date" v-model="selectedDate" />
       </div>
       <div class="table-container">
         <h2>Entradas</h2>
@@ -100,22 +130,33 @@ const valorLiquido = computed(() => {
             </tr>
           </thead>
           <tbody>
-            <tr v-for="(entrada, index) in entradas" :key="index">
-              <td>{{ entrada.categoria }}</td>
-              <td>R${{ entrada.valor }}</td>
-              <td>{{ entrada.data }}</td>
+            <tr v-for="entry in filteredEntries" :key="entry.id">
+              <td>{{ entry.categoria }}</td>
+              <td>{{ entry.valor }}</td>
+              <td>{{ entry.data }}</td>
             </tr>
           </tbody>
         </table>
       </div>
-    </div>
-    <div class="value-box">
-      <h3>Saídas</h3>
-      <div class="value" v-for="(value, key) in saidas" :key="key">{{ key }}: {{ value }}</div>
-    </div>
-  </div>
-  <div class="results">
-    <div class="result">
+      <div class="table-container">
+        <h2>Saídas</h2>
+        <table class="table">
+          <thead>
+            <tr>
+              <th>Data</th>
+              <th>Descrição</th>
+              <th>Valor</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="exit in filteredExits" :key="exit.id">
+              <td>{{ exit.categoria }}</td>
+              <td>{{ exit.valor }}</td>
+              <td>{{ exit.data }}</td>
+            </tr>
+          </tbody>
+        </table>
+        <div class="result">
       <h3>Valor Bruto</h3>
       <span>{{ valorBruto }}</span>
     </div>
@@ -123,7 +164,10 @@ const valorLiquido = computed(() => {
       <h3>Valor Líquido</h3>
       <span>{{ valorLiquido }}</span>
     </div>
+      </div>
+    </div>
   </div>
+  <Footerbar/>
 </template>
 
 <style scoped>
